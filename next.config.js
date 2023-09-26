41434bb541dc389d7a1949b3cb2ec68fb9a88b00
Zenkie Bear <zq@zenkie.cn>
2023-09-26 10:53:46 +0800

    style: configured global scss

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
     locales: ['en'],
     defaultLocale: 'en'
+  },
+  sassOptions: {
+    includPaths: [path.join(__dirname, './styles')],
+    prependData: `@use "@/styles/variables";`
   }
 }
 
